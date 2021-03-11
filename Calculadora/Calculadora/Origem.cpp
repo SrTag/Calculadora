f578425ce3a78b954a5c7d5ac99fb4c0a489d0b2
Sr.Tag <jonathanfarias3105@gmail.com>
2021-03-11 14:49:25 -0300

    finalizado
    projeto finalizado

--- Calculadora/Calculadora/Origem.cpp
+++ Calculadora/Calculadora/Origem.cpp
@@ -7,9 +7,13 @@ int main() {
 
 	Calculadora* calc = new Calculadora;
 	cout << " A soma dos Valores 5 + 2, e igual: " << calc->somar(5.0f, 2.0f) << endl;
+	//f_sub mexi nele agora
 	cout << " A subtracao dos Valores 5 - 2, e igual: " << calc->subtrair(5.0f, 2.0f) << endl;
+	//f_multi mexi nele agora
 	cout << " A multiplicacao dos Valores 5 * 2, e igual: " << calc->multi(5.0f, 2.0f) << endl;
+	//f_divi mexi nele agr
 	cout << " A divisao dos Valores 10 / 2, e igual: " << calc->divisao(10.0f, 2.0f) << endl;
+	cout << "\n\n finalizado";
 
 	system("pause");
 
